480c404102d95d1afc3e7d5697eade51bbb94299
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-18 17:06:52 +0100

    implement /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,7 @@ def delSession():
     '''delete session'''
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
+    if user is not None:
         AUTH.destroy_session(user.id)
         return redirect('/')
     else:
