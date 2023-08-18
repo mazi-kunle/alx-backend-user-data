0f3180f1942f66b9930165c30ac322f035acbe37
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-18 17:03:55 +0100

    implement /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''This is a module'''
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -44,6 +44,18 @@ def sessions():
         return resp
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def delSession():
+    '''delete session'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == '__main__':
