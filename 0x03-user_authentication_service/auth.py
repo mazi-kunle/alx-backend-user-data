ef5a41c2653133ac2c139a47d9a19c6e56020aa7
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-18 11:42:46 +0100

    task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/python3
+'''This is a module'''
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    '''returns a satled hash of the input password
+    '''
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
