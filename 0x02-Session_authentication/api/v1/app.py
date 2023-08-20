d8954179618d11e75f287040c56aebdfd0fa5229
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-20 15:54:32 +0100

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,10 +30,11 @@ def beforeRequest():
     '''run before handling any request
     '''
     excluded_path = ['/api/v1/status/', '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is not None:
         if (auth.require_auth(request.path, excluded_path)):
-            if auth.authorization_header(request) is None:
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
