e2d2b7c60f292909aafff653e031fccacb6fd28c
mazi-kunle <olakunlemichael644@gmail.com>
2023-10-09 00:02:56 +0100

    complete all tasks

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -19,5 +19,4 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     '''
     checks the validity of a password
     '''
-    print(hash_password(password))
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
